d8a16b8ae1b90794939e2fb40676a3f6153ebcbe
Karolina Kaczmarska <kaczmarska.kar@gmail.com>
2019-09-21 15:43:34 +0100

    created questionsList file

---
+++ client/src/components/QuestionsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+      <h2>Questions</h2>
+    <div> 
+    <question-info v-for="(question, index) in questions" :key="index" :question="question"/> 
+    </div>
+      </section>
+</template>
+
+<script>
+import QuestionInfo from "./components/QuestionInfo";
+
+export default {
+name: 'question-list',
+props: ['questions'],
+components: {
+    'question-info': QuestionInfo
+}
+}
+</script>
+
+<style>
+
+</style>
