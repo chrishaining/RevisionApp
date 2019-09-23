039719e7c1fbf54edc59fd8988f2e06da19c7242
Karolina Kaczmarska <kaczmarska.kar@gmail.com>
2019-09-23 16:49:39 +0100

    added placeholder in update form

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <add-question-form/>
     <questions-list :questions="questions"/>
     <question-info :question="selectedQuestion"/>
-    <question-update-form :selected-question="selectedQuestion" />
+    <question-update-form v-if="selectedQuestion" :selected-question="selectedQuestion" />
   </div>
 </template>
 
--- client/src/components/QuestionUpdateForm.vue
+++ client/src/components/QuestionUpdateForm.vue
@@ -2,13 +2,13 @@
   <form v-on:submit.prevent="handleUpdate">
     <h2>Update Question</h2>
     <label for="question">Question:</label>
-    <input type="text" id="question" v-model="question" required></input>
+    <input type="text" id="question" :placeholder="selectedQuestion.question" v-model="question" required></input>
 
     <label for="answer">Answer:</label>
-    <input type="text" id="answer" v-model="answer" required></input>
+    <input type="text" id="answer" :placeholder="selectedQuestion.answer" v-model="answer" required></input>
 
     <label for="url">Topic:</label>
-    <input type="text" id="topic" v-model="topic"></input>
+    <input type="text" id="topic" :placeholder="selectedQuestion.topic" v-model="topic"></input>
 
     <!-- <label for="url">Add a link:</label>
     <input type="text" id="url" v-model="url"></input> -->
