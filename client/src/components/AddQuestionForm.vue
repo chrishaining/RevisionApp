47ee7e9ebfd1851bc2f7175ceda725f68df3d78b
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-23 11:59:06 +0100

    adds a form for adding new question-cards

---
+++ client/src/components/AddQuestionForm.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <form class="" action="index.html" method="post">
+    <h1>Add a new card</h1>
+    <label for="question">Add a question</label>
+    <input type="text" v-model="question" required>
+    <label for="answer">Add an answer</label>
+    <input type="text" v-model="answer" required>
+    <label for="url">Add a URL</label>
+    <input type="text" v-model="url">
+    <!-- <label for="topic">Add a topic</label>
+    <input type="text" v-model="topic"> -->
+  </form>
+</template>
+
+<script>
+
+import {eventBus} from '@/main';
+
+export default {
+  name: "add-question-form",
+  data() {
+    return {
+      question: "",
+      answer: "",
+      url: "",
+      topic: ""
+    };
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
