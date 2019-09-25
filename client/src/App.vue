2a7d2408bc8b2b2828b5b4f6b30c2203d2920421
Davide Campagna <gdavidecampagna@gmail.com>
2019-09-25 11:29:38 +0100

    update css

--- client/src/App.vue
+++ client/src/App.vue
@@ -214,8 +214,9 @@ body {
 	width: 100%;
 	height: 100vh;
 	font-family: "Exo", sans-serif;
-	color: #fff;
-	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	color: teal;
+  background-color: white;
+	/* background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); */
 	background-size: 400% 400%;
 	animation: gradientBG 15s ease infinite;
 }
