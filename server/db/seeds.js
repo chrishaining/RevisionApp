855460ce434eb9f226a639f2797f6e1da93fa61d
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-28 14:05:43 +0100

    adds more pda questions

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,48 +7,56 @@ db.questions.insertMany([
     "url": "https://kissflow.com/rad/rapid-application-development/",
     "topic": "PDA"
   },
+
   {
     "question": "What is the Waterfall model?",
     "answer": "The traditional software development model. The stages are: requirements analysis; design; development; testing; maintenance.",
     "url": "https://en.wikipedia.org/wiki/Waterfall_model",
     "topic": "PDA"
   },
+
   {
     "question": "What is an expression? Give an example.",
     "answer": "In most cases I've seen, it's the code to the left of the = sign in a statement. The expression is equal to a value. For instance, in '3 + 5 = 8', '3 + 5' is the expression.",
     "url": "https://www.webopedia.com/TERM/E/expression.html",
     "topic": "PDA"
   },
+
   {
     "question": "What is a statement? Give an example.",
     "answer": "An expression plus a value, for example '1 + 2 = 3'.",
     "url": "https://en.wikipedia.org/wiki/Statement_(computer_science)",
     "topic": "PDA"
   },
+
   {
     "question": "What is the difference between an expression and a statement? Give examples.",
     "answer": "A statement is made up of an expression plus a value. For instance, in the statement '1+1=2', '1+1' is the expression and '2' is the value.",
     "url": "https://en.wikipedia.org/wiki/Statement_(computer_science)",
     "topic": "PDA"
   },
+
   {
     "question": "What is iteration? Give an example.",
     "answer": "A loop that goes through instructions ",
     "url": "https://prezi.com/tvoflvm-1xf_/p3-explain-sequence-selection-and-iteration-as-used-in-computing/",
     "topic": "PDA"
   },
+
   {
     "question": "What is selection? Give an example.",
     "answer": "One of the three basic programming structures. Selection is when a question is answered and there may be more than one outcome.",
     "url": "https://prezi.com/tvoflvm-1xf_/p3-explain-sequence-selection-and-iteration-as-used-in-computing/",
     "topic": "PDA"
   },
+
   {
-    "question": "What is sequence? Give an example.",
+    "question": "What is a sequence? Give an example.",
     "answer": "A set of instructions in order.",
     "url": "https://www.webopedia.com/TERM/S/sequence.html",
     "topic": "PDA"
   },
+
   {
     "question": "What method do you use to make a string all lower case?",
     "answer": ".toLowerCase()",
@@ -119,10 +127,9 @@ db.questions.insertMany([
     "topic": "PDA"
   },
 
-
   {
-    "question": "Which of the following are advantages of an Agile approach? A: Working software is delivered frequently; B: late changes in requirements are welcomed; C: The project can easily get taken off track; D: No working software is produced until late during the life cycle; E: Regular adaptation to changing circumstances",
-    "answer": "A, E. B is also possible ",
+    "question": "Which of the following are advantages of an Agile approach? A: Working software is delivered frequently; B: late changes in requirements are welcomed; C: The project can easily get taken off track; D: No working software is produced until late during the life cycle; E: Regular adaptation to changing circumstances.",
+    "answer": "A, E. B is also possible",
     "url": "https://www.agilealliance.org/agile101/",
     "topic": "PDA"
   },
@@ -140,7 +147,7 @@ db.questions.insertMany([
     "url": "https://practicingruby.com/articles/qualities-of-great-software",
     "topic": "PDA"
   },
-
+  
   {
     "question": "Which of these are functional requirements? A: certification requirements; B: Authentication; C: Capacity; D: Recoverability; E: Administrative functions",
     "answer": "Certification requirements; Authentication; Administrative functions. Capacity and Recoverability are non-functional requirements.",
@@ -160,6 +167,6 @@ db.questions.insertMany([
     "answer": "Complex; expensive; too much is dependent on risk analysis; difficulty in time management",
     "url": "https://www.geeksforgeeks.org/software-engineering-spiral-model/",
     "topic": "PDA"
-  },
+  }
 
 ])
